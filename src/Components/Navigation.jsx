e357e277bf3bc2d25f6a7e4b1ccc58f32ba51bd6
Mukul <mukul.bairwa@infilytics.in>
2021-12-20 09:38:34 +0530

    Fixes

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,10 +3,13 @@ import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 import image3 from "../image3.png";
 import {FiSearch} from "react-icons/fi"
+import {IoNotificationsCircleOutline} from "react-icons/io5"
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
 const useStyles = makeStyles({
     rootnav:{
         borderBottom:'.1rem solid #e8e8e8',
-        padding:'.6rem',
+        padding:'1rem',
         display:'flex',
     },
     logo:{
@@ -19,7 +22,7 @@ const useStyles = makeStyles({
         width:"25%",
         borderRadius:"1.5rem",
         paddingLeft:"2rem",
-        marginLeft:"1.5rem", 
+        marginLeft:"5.2rem", 
         backgroundColor:"#e0e0e0",
         color:'gray',
         outline:'none',
@@ -37,13 +40,37 @@ const useStyles = makeStyles({
         color:"#a5a5a5",
         fontSize:'.8rem',
     },
+    notification:{
+        listStyle:"none",
+        paddingRight:"1rem",
+        fontWeight:"600",
+        color:"#a5a5a5",
+        fontSize:'2rem',
+        marginTop:"-.5rem",
+    },
     llistActive:{
         listStyle:"none",
         paddingRight:"1rem",
         fontWeight:"600",
         color:"#467cce",
         fontSize:'.8rem',
-    }
+    },
+    fbtn:{
+        background: 'white',
+        color: ' #4cb5ee ',
+        borderRadius:'1rem',
+        width:"11%",
+        padding:'0rem',
+        fontWeight:'600',
+        border:".1rem solid #4cb5ee  ",
+        marginTop:"-.5rem",
+    },
+    avatar:{
+        borderRadius:'50%',
+        width:'70%',
+        marginLeft:".2rem",
+        
+    },
 })
 const Navigation = () => {
     const classes= useStyles();
@@ -59,7 +86,15 @@ const Navigation = () => {
                 <li className={classes.llist}>Activity</li>
                 <li className={classes.llist}>How it Works</li>
                 <li className={classes.llist}>Community</li>
+                <IoNotificationsCircleOutline  className={classes.notification}/>
+                <Button size="small" className={classes.fbtn}>0 Token </Button>
+               
             </ul>
+            <div className="avatar_nav">
+            <Avatar className={classes.avatar}></Avatar>
+            </div>
+            
+            
         </Container>
     )
 }
